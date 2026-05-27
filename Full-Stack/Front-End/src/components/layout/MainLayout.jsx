37d68baa69eb70d74079ffa2fb2a913f9f1a3791
deladeliansyah <dela10223058@student.sttcipasung.ac.id>
2026-05-27 22:18:11 +0700

    update front-end

--- Full-Stack/Front-End/src/components/layout/MainLayout.jsx
+++ Full-Stack/Front-End/src/components/layout/MainLayout.jsx
@@ -1,19 +1,38 @@
-// src/components/layout/MainLayout.jsx
-import { Outlet } from 'react-router-dom';
-import Navbar from './Navbar';
-import Footer from './Footer';
+import { useState } from "react";
 
-// Outlet = placeholder, diisi oleh halaman yang aktif
-const MainLayout = () => {
+import Sidebar from "../ui/Sidebar";
+import Navbar from "../ui/Navbar";
+
+const MainLayout = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
-    <div className="flex min-h-screen flex-col bg-gray-50">
-      <Navbar />
+    <div className="min-h-screen bg-[#f4fbfb] relative overflow-hidden">
 
-      <main className="flex-1 px-4 py-6 md:px-8">
-        <Outlet />
-      </main>
+      {/* Background */}
+      <div className="absolute top-0 left-0 w-96 h-96 bg-teal-300/20 blur-3xl rounded-full -translate-x-1/2 -translate-y-1/2" />
 
-      <Footer />
+      <div className="absolute bottom-0 right-0 w-[500px] h-[500px] bg-cyan-300/20 blur-3xl rounded-full translate-x-1/3 translate-y-1/3" />
+
+      {/* Sidebar */}
+      <Sidebar
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+
+      {/* Main */}
+      <div className="lg:ml-72 min-h-screen flex flex-col relative z-10">
+
+        {/* Navbar */}
+        <Navbar
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+
+        {/* Content */}
+        <main className="flex-1 p-4 md:p-8">
+          {children}
+        </main>
+      </div>
     </div>
   );
 };
