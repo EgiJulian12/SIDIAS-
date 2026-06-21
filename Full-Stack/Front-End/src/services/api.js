f78ec5eeeb4dcf2425fb707c35720260987f6ed8
egijulian12 <julian10223035@student.sttcipasung.ac.id>
2026-06-21 19:55:08 +0700

    api.js

--- Full-Stack/Front-End/src/services/api.js
+++ Full-Stack/Front-End/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '/api',
+  baseURL: 'https://sidias-api.vercel.app/api',
 });
 
 // Interceptor untuk menyisipkan token JWT secara otomatis
